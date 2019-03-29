45a6fac27dc873cd50d64e0a64df2c67f301a0ec
simoncobb1966 <simoncobb1966@gmail.com>
2019-03-29 16:14:22 +0000

    Completed V2 - used Math.ceiling on the bus numbers task to round up

--- challenges/week2.js
+++ challenges/week2.js
@@ -7,21 +7,21 @@ function getFillings(sandwich) {
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
-  return (person.city ==="Manchester")
+  return (person.city === "Manchester")
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
-  return (Math.round((people/40) + 0.49))
+ return Math.ceil(people/40)
 }
 
 function countSheep(arr) {
   if (!arr) throw new Error("arr is required");
   // Your code here!
   var count = 0
-  arr.forEach(function(elem){
-count = count + (elem === "sheep")
+  arr.forEach(function (elem) {
+    count = count + (elem === "sheep")
   })
   return count
 }

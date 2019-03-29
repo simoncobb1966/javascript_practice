dbc3f8a9e780e3b66d0e9639c652293e9f16f097
simoncobb1966 <simoncobb1966@gmail.com>
2019-03-29 14:00:46 +0000

    Get Bus Numbers Complete

--- challenges/week2.js
+++ challenges/week2.js
@@ -1,16 +1,19 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+  return sandwich.fillings
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  return (person.city ==="Manchester")
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  return (Math.round((people/40) + 0.49))
 }
 
 function countSheep(arr) {
